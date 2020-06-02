d5b8b8b7298895b3280179fcf1e917bffc0652a7
Sristi Chowdhury <53037053+Sristi27@users.noreply.github.com>
2020-06-03 00:49:20 +0530

    Update Day 2.cpp

--- Leetcode June challenge/Day 2.cpp
+++ Leetcode June challenge/Day 2.cpp
@@ -1,1 +1,15 @@
-
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+  
+//c++ solution
+  
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+       ListNode*temp=node->next;
+        node->val=temp->val;
+        node->next=temp->next;
+        //e(temp);
+    }
+};
+  
