5ec8ff824e90d4e94c9b8da301e02d4228538592
Sristi Chowdhury <53037053+Sristi27@users.noreply.github.com>
2021-01-20 23:30:54 +0530

    Valid Parantheses

--- January Leet Coding Challenge/Day 20.cpp
+++ January Leet Coding Challenge/Day 20.cpp
@@ -1,4 +1,4 @@
-<h1>Valid Parentheses</h1>
+Valid Parentheses
 Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 An input string is valid if:
